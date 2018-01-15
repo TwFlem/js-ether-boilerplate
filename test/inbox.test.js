d159aa96d8ebb795787f2c7f36ef0edfca714b52
Trenton w Fleming <trenflem@gmail.com>
2018-01-14 20:05:46 -0500

    fixed provider, added a default message text check

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -3,19 +3,25 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const { interface, bytecode } = require('../compile');
 
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 
 let accounts;
 let inbox;
+const defaultMessage = 'hello world!';
 beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
   inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['hello world!'] })
+    .deploy({ data: bytecode, arguments: [defaultMessage] })
     .send({ from: accounts[0], gas: '1000000' })
+  inbox.setProvider(provider);
 });
 
 describe('Inbox', () => {
   it('deploys a contract', () => {
     assert.ok(inbox.options.address);
+  });
+  it('deployed contract has a default message', async () => {
+    assert.ok(await inbox.methods.message().call() === defaultMessage);
   });
 });
